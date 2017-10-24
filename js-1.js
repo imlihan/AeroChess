99324b0071d2aab0ee472c765ffebe5eb8b936a2
lukejohnlayson <30330558+lukejohnlayson@users.noreply.github.com>
2017-10-24 19:34:06 +1300

    Update js-1.js

--- js-1.js
+++ js-1.js
@@ -1,12 +1,21 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 89 || document.documentElement.scrollTop > 89) {
-        document.getElementById("topbtn").style.display = "block";
-    } else {
-        document.getElementById("topbtn").style.display = "none";
-    }
-}
+$(document).ready(function(){
+	
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+	
+});
 
 function topFunction() {
     document.body.scrollTop = 0;
