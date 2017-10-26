bc5c03e667b6547fe299d0c38aaf668810139401
lukejohnlayson <30330558+lukejohnlayson@users.noreply.github.com>
2017-10-26 14:16:50 +1300

    Create js-3.js

---
+++ js-3.js
@@ -0,0 +1,14 @@
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+document.getElementById("main").style.marginLeft= "0";
+}
