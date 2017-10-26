4fdea76ad1fe2a36cf727f76e63b1c2b4654d47a
lukejohnlayson <30330558+lukejohnlayson@users.noreply.github.com>
2017-10-26 14:15:24 +1300

    Update js-1.js

--- js-1.js
+++ js-1.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	
 	//Check to see if the window is top if not then display button
 	$(window).scroll(function(){
-		if ($(this).scrollTop() > 100) {
+		if ($(this).scrollTop() > 20) {
 			$('.scrollToTop').fadeIn();
 		} else {
 			$('.scrollToTop').fadeOut();
