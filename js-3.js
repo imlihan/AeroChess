7caa5769ca796eb310750961db647fdcfa32ea38
lukejohnlayson <30330558+lukejohnlayson@users.noreply.github.com>
2017-10-31 19:36:36 +1300

    Update js-3.js

--- js-3.js
+++ js-3.js
@@ -8,7 +8,12 @@ function openNav() {
     document.getElementById("main").style.marginLeft = "250px";
 }
 
-function closeNav() {
+function closeNav(){
     document.getElementById("mySidenav").style.width = "0";
 document.getElementById("main").style.marginLeft= "0";
+}
+
+function easter() {
+    var audio = document.getElementById;
+    audio.easter();
 }
